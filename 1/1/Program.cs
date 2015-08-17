db01bd16e69a53e2085072a973d14a8985ef3df3
peter dimitrov <structural.database@gmail.com>
2015-08-18 01:23:54 +0300

    Нов ред в Хелло аула

---
+++ 1/1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Application
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.ReadKey ();
+		}
+	}
+}
